7262ab52851f586cc5fc0d1c0d3d39e85a181c2e
devillroyal <deepakyadav4014@gmail.com>
2025-06-10 22:20:45 +0530

    style: search component conversion complete

--- app/search/[slug]/page.jsx
+++ app/search/[slug]/page.jsx
@@ -1,3 +1,4 @@
+import { Construction, Search } from "lucide-react";
 import SearchComponent from "../../../components/SearchComponent";
 
 export default async function SearchPage({ params }) {
@@ -10,9 +11,21 @@ export default async function SearchPage({ params }) {
 
 return(
       <>
-          <div  className="border p-2 md:p-2 xl:p-5">
-            <div className="grid border w-full grid-cols-2 md:grid-cols-4 xl:grid-cols-7 gap-2">
-              {searchData.map((item, index)=>{ return (<div key={item.id} className="grid border grid-rows-1">
+         <div className="flex gap-3 text-white items-center justify-center md:text-lg xl:text-lg h-auto w-full p-5">
+          {searchData.length === 0 ?
+          <Construction size={20}/> :
+          <Search size={20}/>
+           }
+          <div className="flex flex-row">
+            {searchData.length === 0 ? 
+            <p>Sorry could not find result for <span className="text-blue-600 text-wrap font-bold">"{slug}"</span> </p> :
+            <p>Search result for <span className="text-blue-600 text-wrap font-bold">"{slug}"</span> </p>
+            }
+          </div>
+         </div>
+         <div  className="p-2 md:p-2 xl:p-5">
+            <div className="grid w-full grid-cols-2 md:grid-cols-4 xl:grid-cols-7 gap-2">
+              {searchData.map((item, index)=>{ return (<div key={item.id} className="grid grid-rows-1">
                 <SearchComponent
                 name={item.name}
                 id={item.id}
